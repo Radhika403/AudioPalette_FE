a7c74df0025ae86cb8090bfb33f0960f8b4822e0
Radhika403 <radhikabansal403@gmail.com>
2023-01-26 23:29:03 +0530

    ui for ladning page

--- src/Components/Opening.js
+++ src/Components/Opening.js
@@ -10,11 +10,30 @@ function Opening(){
     }
     return(
         <div>
-           <div className="opening_center">
+           {/* <div className="opening_center">
                 <h1>Audio Palette</h1>
                 <h2> How Bad is your Music Taste? <br /> Analyse your music and streaming habits with us</h2>
 
                     <button onClick={handleClick}> Find Out </button>
+            </div> */}
+            <div className="for-justify">
+                <div className="content left">
+                    <h1> Audio </h1>
+                    <h1> <i>Palette</i></h1>
+                    <p>
+                        <i>
+                            How bad is your music taste? Analyze your music with us!
+                        </i>
+                    </p>
+                    <button onClick={handleClick}><i> Get Started </i></button>
+                </div>
+
+                <div className="content right content-right">
+                    <img id="object-position" src={require("./Pictures/ts1.webp")} width="150" height="150" />
+                    <img id="object-position" src={require("./Pictures/sm.png")} width="150" height="150" />
+                    <img id="object-position" src={require("./Pictures/or.webp")} width="150" height="150" />
+                    <img id="object-position" src={require('./Pictures/hs.webp')} width="150" height="150" />
+                </div>
             </div>
         </div>
     )
